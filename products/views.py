1b832bccda8ced78e565e96a0625d5c5a29fac59
James cutmore <robcutmore36@gmail.com>
2023-08-05 20:10:02 +0000

    views.py updated

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import products
 
-# Create your views here.
+
+
+
+def all_products(request):
+    """ A view to show all products , sorting and searching queries """
+
+    products = products.objects . all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html' , context)
+
+
+
+
