2a7e524743b19e1894b9d47b5591f1a0607c1be4
James cutmore <robcutmore36@gmail.com>
2023-08-11 14:27:39 +0000

    base css pointer added,main-nav adjusted

--- smartgym/settings.py
+++ smartgym/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-jamesrcutmore-smartgym-07qqybfxuj7.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['8000-jamesrcutmore-smartgym-07qqybfxuj7.ws-eu103.gitpod.io']
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-jamesrcutmore-smartgym-07qqybfxuj7.ws-eu102.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://8000-jamesrcutmore-smartgym-07qqybfxuj7.ws-eu103.gitpod.io']
 
 
 # Application definition
