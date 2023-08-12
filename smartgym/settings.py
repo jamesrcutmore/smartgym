761e503061170f52fda070bfd41b513655b2cb18
James cutmore <robcutmore36@gmail.com>
2023-08-12 17:31:32 +0000

    shopping bag app and base.html update base,mtop

--- smartgym/settings.py
+++ smartgym/settings.py
@@ -82,7 +82,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                # 'bag.contexts.bag_contents',
+                'bag.contexts.bag_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
