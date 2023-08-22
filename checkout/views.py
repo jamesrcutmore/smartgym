ab76235920a14616701d211b88347c4754d6a380
James cutmore <robcutmore36@gmail.com>
2023-08-22 16:52:06 +0000

    checkout success and overlay

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 from .forms import OrderForm
+from products.models import Product
+from .models import OrderLineItem,Order
 
 from bag.contexts import bag_contents
 
