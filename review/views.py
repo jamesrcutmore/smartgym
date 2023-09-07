0871e5856c247e6164d4bdd9fcb7187244fd4afa
James cutmore <robcutmore36@gmail.com>
2023-09-07 11:57:38 +0000

    review url paterns

--- review/views.py
+++ review/views.py
@@ -12,19 +12,21 @@ from django.urls import reverse
 def rate(request, id):
     product = Product.objects.get(id=id)
     form = ReviewForm(request.POST or None)
-    
+
     if form.is_valid():
         user = request.user
         comment = request.POST.get('comment')
         rating = request.POST.get('rating')
-        review = Review(user=user, product=product, comment=comment, rating=rating)
+        review = Review(user=user, product=product,
+                        comment=comment, rating=rating)
         review.save()
         return redirect('success')
-    
+
     context = {
-        "form": form
+        "form": form,
+        "product": product
     }
-    
+
     return render(request, 'review/rate.html', context)
 
 
